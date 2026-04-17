199dcb68df59dbd3fc1e33e095d4b919244c2171
ahri032 <kljjgu02@gmail.com>
2026-04-17 12:53:26 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,12 @@
-const CACHE_NAME = 'rotomuwiki-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-];
-
-// 설치: 핵심 파일 캐싱
-self.addEventListener('install', e => {
+// Service Worker 비활성화
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
-
-// 활성화: 구버전 캐시 삭제
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-// 요청: 캐시 우선, 없으면 네트워크
-self.addEventListener('fetch', e => {
-  // pkmnchamps.github.io 스프라이트는 네트워크 우선 (이미지 업데이트 반영)
-  if (e.request.url.includes('pkmnchamps.github.io')) {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-          return res;
-        })
-        .catch(() => caches.match(e.request))
-    );
-    return;
-  }
-
-  // 나머지는 캐시 우선
-  e.respondWith(
-    caches.match(e.request)
-      .then(cached => cached || fetch(e.request)
-        .then(res => {
-          if (res.ok) {
-            const clone = res.clone();
-            caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-          }
-          return res;
-        })
-      )
-  );
+// 모든 요청을 캐시 없이 네트워크에서 직접 가져옴
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
